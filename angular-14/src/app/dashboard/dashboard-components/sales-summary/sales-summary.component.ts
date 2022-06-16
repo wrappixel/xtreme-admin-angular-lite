cb254c0064b41dfe3023c608f7ade49cdad1fedf
javiyadeep <patelaleis@gmail.com>
2022-06-16 16:49:41 +0530

    .

---
+++ angular-14/src/app/dashboard/dashboard-components/sales-summary/sales-summary.component.ts
@@ -0,0 +1,90 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ChartComponent,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexLegend,
+  ApexXAxis,
+  ApexTooltip,
+  ApexTheme,
+  ApexGrid
+} from 'ng-apexcharts';
+
+export type salesChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  stroke: any;
+  theme: ApexTheme;
+  tooltip: ApexTooltip;
+  dataLabels: ApexDataLabels;
+  legend: ApexLegend;
+  colors: string[];
+  markers: any;
+  grid: ApexGrid;
+};
+
+@Component({
+  selector: 'app-sales-summary',
+  templateUrl: './sales-summary.component.html'
+})
+export class SalesSummaryComponent implements OnInit {
+
+  @ViewChild("chart") chart: ChartComponent = Object.create(null);
+  public salesChartOptions: Partial<salesChartOptions>;
+  constructor() {
+    this.salesChartOptions = {
+      series: [
+        {
+          name: "Iphone 13",
+          data: [0, 31, 40, 28, 51, 42, 109, 100],
+        },
+        {
+          name: "Oneplue 9",
+          data: [0, 11, 32, 45, 32, 34, 52, 41],
+        },
+      ],
+      chart: {
+        fontFamily: 'Nunito Sans,sans-serif',
+        height: 250,
+        type: 'area',
+        toolbar: {
+          show: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth',
+        width: '1',
+      },
+      grid: {
+        strokeDashArray: 3,
+      },
+
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "Aug",
+        ],
+      },
+      tooltip: {
+        theme: 'dark'
+      }
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
