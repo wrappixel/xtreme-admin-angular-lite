3b8f3c738810c73a55899107aa403f7fa7a796d2
wrappixelTeam <wrappixelteam.2016@gmail.com>
2023-06-17 10:03:45 +0530

    ''

---
+++ angular-16/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Routes, RouterModule } from "@angular/router";
+import { NgApexchartsModule } from "ng-apexcharts";
+import { DashboardComponent } from "./dashboard.component";
+import { SalesSummaryComponent } from "./dashboard-components/sales-summary/sales-summary.component";
+import { FeedsComponent } from "./dashboard-components/feeds/feeds.component";
+import { TopSellingComponent } from "./dashboard-components/top-selling/top-selling.component";
+import { TopCardsComponent } from "./dashboard-components/top-cards/top-cards.component";
+import { BlogCardsComponent } from "./dashboard-components/blog-cards/blog-cards.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    data: {
+      title: "Dashboard",
+      urls: [{ title: "Dashboard", url: "/dashboard" }, { title: "Dashboard" }],
+    },
+    component: DashboardComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule.forChild(routes),
+    NgApexchartsModule,
+  ],
+  declarations: [
+    DashboardComponent,
+    SalesSummaryComponent,
+    FeedsComponent,
+    TopSellingComponent,
+    TopCardsComponent,
+    BlogCardsComponent
+  ],
+})
+export class DashboardModule {}
